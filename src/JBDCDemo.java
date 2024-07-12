71d71ce2cac76ef8d45bdf2516c99c54020c85cf
puyyy <116352262+reppyassiddiq@users.noreply.github.com>
2024-07-12 22:22:30 +0700

    JBDCDemo.java

--- src/JBDCDemo.java
+++ src/JBDCDemo.java
@@ -1,5 +1,28 @@
-public class Main {
+import java.sql.*;
+
+public class JBDCDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url="jdbc:mysql://localhost:3306/jdbcdemo";
+        String username="root";
+        String password="";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connection= DriverManager.getConnection(url,username,password);
+
+            Statement statement=connection.createStatement();
+
+            ResultSet resultSet=statement.executeQuery("select * from student");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getInt(3));
+            }
+
+            connection.close();
+        }
+
+        catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
